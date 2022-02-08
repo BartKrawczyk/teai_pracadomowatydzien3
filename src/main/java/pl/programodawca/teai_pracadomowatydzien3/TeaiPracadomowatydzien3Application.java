0802eb619f2b26caa384e48a2eb9f3de3f2a8b9f
Bart Krawczyk <bartek.krawczyk@interia.pl>
2022-02-08 21:17:08 +0100

    Initial commit

---
+++ src/main/java/pl/programodawca/teai_pracadomowatydzien3/TeaiPracadomowatydzien3Application.java
@@ -0,0 +1,13 @@
+package pl.programodawca.teai_pracadomowatydzien3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowatydzien3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaiPracadomowatydzien3Application.class, args);
+	}
+
+}
